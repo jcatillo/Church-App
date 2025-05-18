1dc9b0ebfd0b86e80bdb9ddcb404061fed806c17
jcatillo <jcatillo1121@gmail.com>
2025-05-18 11:21:15 +0800

    feat: added backend to the db

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getFirestore} from 'firebase/firestore'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBVaufD4569DohlX_b3Ss4THd3sBe5w_m4",
   authDomain: "church-5d383.firebaseapp.com",
@@ -16,6 +11,6 @@ const firebaseConfig = {
   measurementId: "G-9W97EBHG20"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -39,7 +39,7 @@ export function Schedule() {
 
     plugins:[
         createEventModalPlugin()
-    ]
+    ],
   })
 
   return (
